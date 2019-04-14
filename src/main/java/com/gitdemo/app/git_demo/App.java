04e1dd1e88d46ba347f96f561507b53af2931e6a
AshishChanne <ashish.channe@s-force.org>
2019-04-14 17:38:06 +0530

    added constant class in app.java

--- src/main/java/com/gitdemo/app/git_demo/App.java
+++ src/main/java/com/gitdemo/app/git_demo/App.java
@@ -8,9 +8,9 @@ public class App
 {
 	public void addData(String data)
 	{
-		System.out.println(" data "+data);
 		
-		if(data.equals("ashish"))
+		
+		if(data.equals(Constant.DATA_TYPE))
 		{
 			System.out.println("Welcome");
 		}
---
+++ src/main/java/com/gitdemo/app/git_demo/Constant.java
@@ -0,0 +1,6 @@
+package com.gitdemo.app.git_demo;
+
+public class Constant {
+
+	public static String DATA_TYPE="Welcome ashish";
+}
